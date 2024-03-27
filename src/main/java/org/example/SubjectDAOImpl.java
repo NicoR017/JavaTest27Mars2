cfb2a67575f8505509493b251e76ef44f2021b52
NicoR017 <nicolas.rasteiro@gmail.com>
2024-03-27 19:47:21 +0100

    first commit

---
+++ src/main/java/org/example/SubjectDAOImpl.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class SubjectDAOImpl implements StudentDAO{
+    @Override
+    public Boolean addStudent(Student student) throws ClassNotFoundException, SQLException {
+        return null;
+    }
+
+    @Override
+    public void deleteStudent(int id) throws ClassNotFoundException, SQLException {
+
+    }
+
+    @Override
+    public List<Student> getAllStudents() throws ClassNotFoundException, SQLException {
+        return null;
+    }
+
+    @Override
+    public void updateStudent(Student student) throws ClassNotFoundException, SQLException {
+
+    }
+
+    @Override
+    public Student findStudentById(Student student) throws ClassNotFoundException, SQLException {
+        return null;
+    }
+}
